09dffed1c2a57ebbab36da11e0aaaea144c1583a
DonJul34 <jules.galian@gmail.com>
2024-12-03 11:06:04 +0100

    Exercise django manage model

--- books/models.py
+++ books/models.py
@@ -8,6 +8,27 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+class BookManager(models.Manager):
+    def filter_books(self, title=None, author=None, year=None, genre=None, sort_by=None):
+        """
+        Filtre les livres en fonction des paramètres fournis.
+        """
+        books = self.all()
+
+        if title:
+            books = books.filter(title__icontains=title)
+        if author:
+            books = books.filter(author__name__icontains=author)
+        if year and year.isdigit():
+            books = books.filter(publication_year__gte=int(year))
+        if genre:
+            books = books.filter(genres__name__icontains=genre)
+
+        if sort_by in ['title', 'author__name', 'publication_year']:
+            books = books.order_by(sort_by)
+
+        return books
+
 class Publisher(models.Model):
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=50)
@@ -21,7 +42,7 @@ class Book(models.Model):
     publisher = models.OneToOneField(Publisher, on_delete=models.CASCADE)  # One-to-One
     publication_year = models.IntegerField()
     genres = models.ManyToManyField('Genre', related_name="books")  # Many-to-Many
-
+    objects = BookManager()
     def __str__(self):
         return f"{self.title} by {self.author.name} ({self.publication_year})"
 
